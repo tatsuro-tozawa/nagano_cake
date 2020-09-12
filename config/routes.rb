ab2903a931ed2daf4138b05f1350a5f08ccaa5f6
vagrant <vagrant@localhost.localdomain>
2020-09-12 12:53:05 +0000

    [Update]200912

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+
+  root to: 'homes#top'
+
+  namespace :admins do
+    get 'homes/top' => 'homes#top', as:'top'
+  end
+  devise_for :admins
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
